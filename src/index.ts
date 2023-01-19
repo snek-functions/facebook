5458c40fe1caa68c6672fa68ce36b0c45e19647b
Florian Kleber <kleberbaum@erebos.xyz>
2023-01-19 12:32:09 +0000

    feat: add functions to index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export {default as getPageFeed} from './getPageFeed.js'
+export {default as managePageFeed} from './managePageFeed.js'
+export {default as login} from './login.js'
+export {default as blockPost} from './blockPost.js'
+export {default as unblockPost} from './unblockPost.js'
 
 // SPDX-License-Identifier: (EUPL-1.2)
 // Copyright © 2019-2022 snek.at
