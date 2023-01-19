db115763b7335ab5885ce877ca54ef2f3911d575
Florian Kleber <kleberbaum@erebos.xyz>
2023-01-19 09:08:08 +0000

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,19 @@
+import cors from 'cors'
+import express from 'express'
+
+import {ConfigureApp} from '@snek-at/functions'
 import getServerlessApp from '@snek-at/functions/dist/server/getServerlessApp.js'
+
+export const configureApp: ConfigureApp = app => {
+  app.use((req, res, next) => {
+    return cors({
+      origin: true,
+      credentials: true
+    })(req, res, next)
+  })
+
+  app.use(express.urlencoded())
+}
 
 export async function handler(event: Object, context: Object) {
   return await getServerlessApp({
